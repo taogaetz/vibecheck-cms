ab8793e93e17492123a461d9e36816c73be32b8a
taogaetz <59668529+taogaetz@users.noreply.github.com>
2024-07-08 16:04:33 -0400

    added posts collection

---
+++ src/collections/Posts.ts
@@ -0,0 +1,27 @@
+import { CollectionConfig } from 'payload/types'
+
+const Posts: CollectionConfig = {
+  slug: 'posts',
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'content',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'author',
+      type: 'relationship',
+      relationTo: 'users',
+    },
+  ],
+}
+
+export default Posts
